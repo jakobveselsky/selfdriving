e90d865935df6c98b4d6e07ad71d2bd610b7705e
jakobveselsky <jveselsky@luc.edu>
2021-05-21 14:08:53 -0500

    creation of catkin workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/selfdriving/PCstream/img/selfdriving/selfdriving/src
+# Build directory: /home/selfdriving/PCstream/img/selfdriving/selfdriving/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/selfdriving/PCstream/img/selfdriving/selfdriving/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/selfdriving/PCstream/img/selfdriving/selfdriving/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/selfdriving/PCstream/img/selfdriving/selfdriving/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/selfdriving/PCstream/img/selfdriving/selfdriving/src:$ROS_PACKAGE_PATH"
